ad424cc09e7fc3a592a1653a3d627581e1ca2342
sargisheiser <sargis.heiser@gmail.com>
2026-03-27 21:36:28 +0100

    feat: add frontend logger utility with log levels

---
+++ frontend/src/utils/logger.js
@@ -0,0 +1,37 @@
+const LOG_LEVELS = {
+  debug: 0,
+  info: 1,
+  warn: 2,
+  error: 3,
+  silent: 4,
+}
+
+const CURRENT_LEVEL = import.meta.env.PROD ? LOG_LEVELS.warn : LOG_LEVELS.debug
+
+const logger = {
+  debug(...args) {
+    if (CURRENT_LEVEL <= LOG_LEVELS.debug) {
+      console.debug('[HYPERFIT]', ...args)
+    }
+  },
+
+  info(...args) {
+    if (CURRENT_LEVEL <= LOG_LEVELS.info) {
+      console.info('[HYPERFIT]', ...args)
+    }
+  },
+
+  warn(...args) {
+    if (CURRENT_LEVEL <= LOG_LEVELS.warn) {
+      console.warn('[HYPERFIT]', ...args)
+    }
+  },
+
+  error(...args) {
+    if (CURRENT_LEVEL <= LOG_LEVELS.error) {
+      console.error('[HYPERFIT]', ...args)
+    }
+  },
+}
+
+export default logger
